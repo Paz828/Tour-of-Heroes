fdffa90b2ea0590a36ad8f3fcc11f2e2ae7fdb9b
Dorion Boesch <dorion.boesch@gmail.com>
2023-11-08 19:38:16 -0700

    added a hero service and hero details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, HeroesComponent],
+  imports: [BrowserModule, FormsModule, HeroesComponent, HeroDetailComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,16 +1,32 @@
 import { Component } from '@angular/core';
 import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
 @Component({
   standalone: true,
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
-  imports: [NgFor, FormsModule, NgIf, UpperCasePipe],
+  imports: [NgFor, FormsModule, NgIf, UpperCasePipe, HeroDetailComponent],
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  heroes: Hero[] = [];
+  selectedHero?: Hero;
+
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
 }
