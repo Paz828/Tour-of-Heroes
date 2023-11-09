80111a4733d70357ae59ab24dea9b8da26d6d25a
Dorion Boesch <dorion.boesch@gmail.com>
2023-11-08 22:38:38 -0700

    working on messages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MessagesComponent],
   imports: [BrowserModule, FormsModule, HeroesComponent, HeroDetailComponent],
   providers: [],
   bootstrap: [AppComponent],
